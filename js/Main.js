e551e05815a7dc9fdd1ba54d42c19a71af6f54fb
Michiel <Michieldesmet1994@gmail.com>
2020-07-17 20:36:25 +0200

    Sorting the content. Still need to recreate the new content

--- js/Main.js
+++ js/Main.js
@@ -3,16 +3,16 @@ const textArea = document.querySelector('textarea')
 
 input.addEventListener('change', () => {
     let files = input.files;
-
-    if(files.length === 0) return;
+    if (files.length === 0) {
+        return;
+    }
 
     const file = files[0]
+    const reader = new FileReader();
 
-    let reader = new FileReader();
-
-    reader.onload = (e) => {
-        const file = e.target.result;
-        const lines = file.split(/\r\n|\n/);
+    reader.onload = (fileReader) => {
+        const sortedFile = sortTheFile(fileReader.target.result);
+        const lines = sortedFile.split(/\r\n|\n/);
         textArea.value = lines.join('\n');
     }
 
@@ -21,3 +21,24 @@ input.addEventListener('change', () => {
     reader.readAsText(file);
 
 });
+
+function sortTheFile(fileToBeSorted) {
+    let arrayOfSplitString = fileToBeSorted.split(/[{}]/)
+    console.log(arrayOfSplitString);
+
+    let sortedArray = new Array(arrayOfSplitString.length)
+
+    for (const inputElement of arrayOfSplitString) {
+        let splitElement = inputElement.trim().split(/\n/);
+        if (splitElement.length !== 1) {
+            splitElement = splitElement.sort();
+        }
+        sortedArray.push(splitElement)
+    }
+
+    console.log(sortedArray);
+
+    //TODO: Now we need to recreate the new sorted content.
+
+    return fileToBeSorted;
+}
