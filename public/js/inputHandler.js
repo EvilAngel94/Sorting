88a3fdeda6892a5a54b3af3d4cf4ef49f2131ec3
Michiel <Michieldesmet1994@gmail.com>
2020-07-31 15:15:25 +0200

    Refactor - refactored the code a bit, so not everything is inside one JS file

---
+++ public/js/inputHandler.js
@@ -0,0 +1,25 @@
+import { sortTheFile } from "./SortingScript.js"
+
+const input = document.querySelector('input');
+const textArea = document.querySelector('textarea')
+
+input.addEventListener('change', () => {
+    let files = input.files;
+    if (files.length === 0) {
+        return;
+    }
+
+    const file = files[0]
+    const reader = new FileReader();
+
+    reader.onload = (fileReader) => {
+        const sortedFile = sortTheFile(fileReader.target.result);
+        const lines = sortedFile.split(/\r\n|\n/);
+        textArea.value = lines.join('\n');
+    }
+
+    reader.onerror = (e) => alert(e.target.error.name);
+
+    reader.readAsText(file);
+
+});
